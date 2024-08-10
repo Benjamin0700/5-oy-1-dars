13a58c4a2af7a03db8a17ab0f5266e8ae41b748f
Benjamin <RichMan@MacBook-Pro-RichMan.local>
2024-08-10 21:07:48 +0500

    5 oy 1 dars

---
+++ main.js
@@ -0,0 +1,94 @@
+// let son = +prompt(`7 gacha son kiriting`)
+
+// switch (son) {
+//     case 1:
+//         alert("Dushanba ish kuni-1")
+//       break;
+//     case 2:
+//         alert("Seshanba ish kuni-2")
+//       break;
+//     case 3:
+//         alert("Chorshanba ish kuni-3")
+//       break;
+//       case 4:
+//         alert("Payshanba ish kuni-4")
+//       break;
+//     case 5:
+//         alert ("Juma ish kuni-5")
+//       break;
+//     case 6:
+//         alert("Shanba dam olish-1")
+//       break;
+//     case 7:
+//         alert("Yakshanba dam olish-2")
+//         break;
+//         default:
+//          alert("Bunday kun yoq")   
+//          break;
+//   }
+
+// let son = +prompt(`Favfqulotda vaziyatla bolimasi:`)
+
+// switch (son) {
+//     case 101:
+//         alert("Ot ochiruvchilar eshitadi")
+//       break;
+//     case 102:
+//         alert("Melitsita eshitadi")
+//       break;
+//     case 103:
+//         alert("Skoriy xizmati eshitadi")
+//       break;
+//       case 104:
+//         alert("Gaz xizmati eshitadi")
+//       break;
+//     case 1050:
+//         alert ("Qutqaruv xizmati eshitadi")
+//       break;
+//       case +998946187788:
+//         alert ("Emirhan eshitadi")
+//       break;
+//         default:
+//          alert("Bunday son yoq")   
+//          break;
+//   }
+
+// let yosh = +prompt("Yosh kirit:");
+
+// switch (yosh) {
+//     case 16:
+//         alert("Yosh bolasan");
+//         break;
+//     case 18:
+//         alert("Nima gap?");
+//         let gap = prompt("Javob:");
+//         alert("Hayot qanaqa?");
+//         let gap2 = prompt("Javob:");
+//         alert("Uydigila tinchmi?");
+//         let gap3 = prompt("Javob:");
+//         alert("Ustozin kim?");
+//         let gap4 = prompt("Javob:");
+//         alert("Qatta oqisan?");
+//         let gap5 = prompt("Javob:");
+//         alert("Sog bol jgar)");
+//         break;
+//     default:
+//         alert("Yoshingiz mos kelmaydi");
+//         break;
+// }
+
+// let ball = +prompt("Ballingizni kiriting:");
+
+// if (ball >= 85 && ball <= 100) {
+//     alert("Grade A");
+// } else if (ball >= 65 && ball < 85) {
+//     alert("Grade B");
+// } else if (ball >= 45 && ball < 65) {
+//     alert("Grade C");
+// } else if (ball >= 25 && ball < 45) {
+//     alert("Grade D");
+// } else if (ball >= 0 && ball < 25) {
+//     alert("Grade E");
+// } else {
+//     alert("Bunday ball yo'q");
+// }
